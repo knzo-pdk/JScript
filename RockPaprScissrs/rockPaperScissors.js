892ac4979f1b4a607e4d513213259b457a871c3b
Kenzo Pham <khoiduc1997@gmail.com>
2023-02-15 20:31:59 -0500

    I think it's functioning as intended

--- RockPaprScissrs/rockPaperScissors.js
+++ RockPaprScissrs/rockPaperScissors.js
@@ -43,31 +43,39 @@ var playerScore = 0;
 
     //counts no# of time 'i' has to increment. Win or lose is then decided on 'i'
     function playRound(array, digit, playerHand, cpuHand){
+        if(playerScore !== 5 && cpuScore !== 5) {
+            let scoreArr = [];
+            let verdict = "";
+            const container = document.querySelector('#result');
+            const content = document.createElement('div');
+            container.appendChild(content);
+            
 
-        let i = 0;
-        while(cpuHand !== array[i]) {
-            i++;
-            digit++;
+            let i = 0;
+            while(cpuHand !== array[i]) {
+                i++;
+                digit++;
+            }
+
+            if (digit == 2 || digit == -1) {
+                verdict = "[You lose! " + array[i] + " beats " + playerHand + ']';
+                cpuScore++;
+            } else if (digit == 1) {
+                verdict = "[You win! " + playerHand + " beats " + array[i] + ']';
+                playerScore++;
+            } else { // 4 is tie. 3 is tie. 
+                verdict = "[It's a tie " + playerHand + " is tied with " + array[i] + ']';
+            }
+            container.textContent = verdict + "\n" + "Cpu score: " + cpuScore + " Player score: " + playerScore;
+            scoreArr.forEach(announce);       
+        
+            if(cpuScore == 5 && cpuScore > playerScore) {
+                container.textContent = verdict + "Computer won!"
+            } else if (playerScore == 5 && cpuScore < playerScore) {
+                container.textContent = verdict + "You won!"
+            }
         }
-        let verdict = "";
-        if (digit == 2 || digit == -1) {
-            verdict = "You lose! " + array[i] + " beats " + playerHand;
-            cpuScore++;
-        } else if (digit == 1) {
-            verdict = "You win! " + playerHand + " beats " + array[i];
-            playerScore++;
-        } else { // 4 is tie. 3 is tie. 
-            verdict = "It's a tie " + playerHand + " is tied with " + array[i];
-        }
-
-        const container = document.querySelector('#result');
-        const content = document.createElement('div');
-        content.textContent = verdict;
-
-        container.appendChild(content);
-      
-        getComputerChoice();
-        return [cpuScore, playerScore];
+        return;
     }
 
     function announce(item){
@@ -76,33 +84,32 @@ var playerScore = 0;
     
     const btn = document.querySelector('#btn-r'); //reference to button
     btn.addEventListener('click', () => {  //when clicked on, do...
-        if(playerScore !== 5 && cpuScore !== 5) {
+        //if(playerScore !== 5 && cpuScore !== 5) {
             var num = getComputerChoice();
             playerHand = 'Rock';
             let digit = assignDigit(playerHand); //assign a specific digit to the playerHand
             let scoreArr = playRound(rpp, digit, playerHand, computerHand(num));
-            scoreArr.forEach(announce)
-        }    
+        //}    
     });
     const btn1 = document.querySelector('#btn-p');
     btn1.addEventListener('click', () => {  
-        if(playerScore !== 5 && cpuScore !== 5) {
+        //if(playerScore !== 5 && cpuScore !== 5) {
             var num = getComputerChoice();
             playerHand = 'Paper';
             let digit = assignDigit(playerHand);
             let scoreArr = playRound(rpp, digit, playerHand, computerHand(num));
-            scoreArr.forEach(announce)
-        }
+            //scoreArr.forEach(announce)
+        //}
     });
     const btn2 = document.querySelector('#btn-s');
     btn2.addEventListener('click', () => {
-        if(playerScore !== 5 && cpuScore !== 5) {
+        //if(playerScore !== 5 && cpuScore !== 5) {
             var num = getComputerChoice();
             playerHand = 'Scissor';
             let digit = assignDigit(playerHand);
             let scoreArr = playRound(rpp, digit, playerHand, computerHand(num));
-            scoreArr.forEach(announce)
-        }
+            //scoreArr.forEach(announce)
+        //}
     });
 // }
 // play();
